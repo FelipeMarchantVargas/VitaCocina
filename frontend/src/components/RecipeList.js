14b13ddc356eca7de4e0162ae5a6984521e77b1b
SebastianBaeza <sebastian.baezaa@usm.cl>
2024-10-18 16:58:27 -0300

    Correccion de botones y estetica

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import '../stylesheets/RecipeList.css'; // Importa el archivo CSS
+import { useNavigate } from "react-router-dom"; // Si usas react-router-dom para la navegación
+import '../stylesheets/RecipeList.css'; // Importa el CSS
 
 const RecipeList = () => {
   const [recipes, setRecipes] = useState([]);
+  const history = useNavigate(); // Hook para la navegación
 
   useEffect(() => {
     const fetchRecipes = async () => {
@@ -18,16 +20,29 @@ const RecipeList = () => {
     fetchRecipes();
   }, []);
 
+  const navigateToRegister = () => {
+    history("/register");
+  };
+
+  const navigateToLogin = () => {
+    history("/login");
+  };
+
   return (
     <div>
       <h1>Recetas Saludables</h1>
+      
+      <div className="button-container">
+        <button onClick={navigateToRegister}>Registrarse</button>
+        <button onClick={navigateToLogin}>Iniciar Sesión</button>
+      </div>
+
       <div className="recipe-list">
         {recipes.map((recipe) => (
           <div key={recipe._id} className="recipe-card">
             <h1>{recipe.title}</h1>
             <img src={recipe.image} alt={recipe.title} />
             <p>{recipe.description}</p>
-            <typography>Ingredientes:</typography>
             <ol>
               {(recipe.ingredients || []).map((ingredient, index) => (
                 <li key={index}>{ingredient}</li>

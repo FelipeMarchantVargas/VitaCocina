84f9bb5a426a5a7f77f459fde7b0aea5960e489f
FelipeMarchantVargas <felipe.marchantv@sansano.usm.cl>
2024-11-09 19:49:03 -0300

    Update 4- deleteRecipe.cy.js

--- cypress/e2e/2-recipes/4- deleteRecipe.cy.js
+++ cypress/e2e/2-recipes/4- deleteRecipe.cy.js
@@ -29,30 +29,30 @@ describe("Delete Recipe", () => {
   //   password: 'S4f3_p@ssw0rd'
   // };
 
-  /* before(() => {
-      // Log in before running the tests
-      cy.visit('/login');
-      cy.get('input[name="email"]').type(user.email);
-      cy.get('input[name="password"]').type(user.password);
-      cy.get('button[type="submit"]').click();
-      cy.on('window:alert', (str) => {
+  before(() => {
+    // Log in before running the tests
+    cy.visit("/login");
+    cy.get('input[name="email"]').type(user.email);
+    cy.get('input[name="password"]').type(user.password);
+    cy.get('button[type="submit"]').click();
+    /* cy.on('window:alert', (str) => {
         expect(str).to.equal('Logged in successfully');
+      }); */
+    cy.window().then((win) => {
+      cy.request({
+        method: "POST",
+        url: "/api/auth/login",
+        body: {
+          email: user.email,
+          password: user.password,
+        },
+      }).then((response) => {
+        const token = response.body.token;
+        win.localStorage.setItem("authToken", token);
+        win.localStorage.setItem("userName", user.name);
       });
-      cy.window().then((win) => {
-        cy.request({
-          method: 'POST',
-          url: '/api/auth/login',
-          body: {
-            email: user.email,
-            password: user.password
-          }
-        }).then((response) => {
-          const token = response.body.token;
-          win.localStorage.setItem('authToken', token);
-          win.localStorage.setItem('userName', user.name);
-        });
-      });
-    }); */
+    });
+  });
 
   it("should delete the recipe", () => {
     cy.visit("/");

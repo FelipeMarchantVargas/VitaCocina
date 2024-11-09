8011756d65e48c565be9891d1fc50d06c026efa8
FelipeMarchantVargas <felipe.marchantv@sansano.usm.cl>
2024-11-09 14:50:21 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Ignorar excepciones no manejadas
+      on("uncaught:exception", (err, runnable) => {
+        // Devuelve false para evitar que Cypress falle en excepciones no manejadas
+        return false;
+      });
+
+      // Aquí puedes implementar otros listeners de eventos
     },
-    baseUrl: 'http://localhost:3000', // Cambia esto a la URL base de tu aplicación
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: "http://localhost:3000", // Cambia esto a la URL base de tu aplicación
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
   component: {
     devServer: {
